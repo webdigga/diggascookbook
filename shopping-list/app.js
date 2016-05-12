ee06484baeb89ad4d0069830783f9bcb96d5d609
David White <webdigga42@gmail.com>
2016-05-12 21:19:26 +0100

    Grabbing ingredient data from JSON

--- shopping-list/app.js
+++ shopping-list/app.js
@@ -5,9 +5,8 @@ shoppingList.controller('ShoppingListController', [
 	'$http',
 	function($scope, $http) {
 		
-		$http.get('js/json/fruit-veg.json').then(function(data) {
-
-			console.log(data);
+		$http.get('js/json/fruit-veg.json').then(function( fruitVeg ) {
+			$scope.fruitVeg = fruitVeg.data;		
 
 			// TODO - get the map working as per,
 			// https://github.com/shanly-suepaul/angular-flight-site/blob/master/src/services/dataAPI.js
