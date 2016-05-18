12661f6855ab160970666f8b6f5fcad6a6e71849
David White <webdigga42@gmail.com>
2016-05-18 19:36:49 +0100

    Initial map setup

--- shopping-list/app.js
+++ shopping-list/app.js
@@ -1,9 +1,14 @@
 var shoppingList = angular.module('shoppingList', []);
 
 shoppingList.controller('ShoppingListController', ['$scope', '$http', 'dataAPI', function( $scope, $http, dataAPI ) {
-		
-	console.log(dataAPI.getIngredients());
-	$scope.ingredients = dataAPI.getIngredients();
-
 	
+	// Set the scope variable to an empty array
+	$scope.ingredientsList = [];
+
+	// Run the get ingredients function returning ingredientsList as the reference
+	dataAPI.getIngredients().then(function( ingredientsList ) {
+
+		// Now assign all the ingredients into our empty array
+		$scope.ingredientsList = ingredientsList;
+	})
 }]);
--- shopping-list/services/dataAPI.js
+++ shopping-list/services/dataAPI.js
@@ -1,8 +1,20 @@
 shoppingList.service('dataAPI', function( $http ) {
 	return {
 		getIngredients: function() {
-			return $http.get( 'js/json/ingredients.json' ).then(function( ingredients ) {
-				return ingredients.data;
+
+			// Make the call to our ingredients JSON
+			return $http.get( 'js/json/ingredients.json' ).then(function( response ) {
+
+				// Return the data in a map so we can explicitly reference the data
+				return response.data.map(function( ingredient ) {
+					console.log(ingredient.recipes.name);					
+					return {
+						name: ingredient.name,
+						quantity: ingredient.quantity,
+						option: ingredient.option,
+						recipes: ingredient.recipes.name
+					};
+				});
 			});
 		}
 	}
