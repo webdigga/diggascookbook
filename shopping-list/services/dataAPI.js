893303e0d170af5279180bf9312c66632a9dc223
David White <webdigga42@gmail.com>
2016-05-20 18:56:55 +0100

    Adding array for recipe type to JSON and spitting out data

--- shopping-list/services/dataAPI.js
+++ shopping-list/services/dataAPI.js
@@ -4,17 +4,7 @@ shoppingList.service('dataAPI', function( $http ) {
 
 			// Make the call to our ingredients JSON
 			return $http.get( 'js/json/ingredients.json' ).then(function( response ) {
-
-				// Return the data in a map so we can explicitly reference the data
-				return response.data.map(function( ingredient ) {
-					console.log(ingredient.recipes.name);					
-					return {
-						name: ingredient.name,
-						quantity: ingredient.quantity,
-						option: ingredient.option,
-						recipes: ingredient.recipes.name
-					};
-				});
+				return response.data;				
 			});
 		}
 	}
